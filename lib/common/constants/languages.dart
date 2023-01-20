409daa3b8c3466bcf409b02d71f7543eef54b37e
CreedTech <ayodeleayoolaoke4@gmail.com>
2023-01-20 13:02:46 +0100

    first commit

---
+++ lib/common/constants/languages.dart
@@ -0,0 +1,11 @@
+import '../../domain/entities/language_entity.dart';
+
+class Languages {
+  const Languages._();
+
+  static const languages = [
+    LanguageEntity(code: 'en', value: 'English'),
+    LanguageEntity(code: 'vi', value: 'Việt Nam'),
+  ];
+}
+  
